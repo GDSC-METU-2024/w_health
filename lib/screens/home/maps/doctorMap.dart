9bb35592ec34f34028a675a577d5b1b81e3c9068
AlperenOvak <alperen.ovak@metu.edu.tr>
2024-01-29 17:46:25 +0300

    Rating stars in Map

--- lib/screens/home/maps/doctorMap.dart
+++ lib/screens/home/maps/doctorMap.dart
@@ -1,7 +1,7 @@
 import 'package:custom_info_window/custom_info_window.dart';
-import 'package:easy_localization/easy_localization.dart';
 import 'package:firebase_core/firebase_core.dart';
 import "package:flutter/material.dart";
+import 'package:flutter_rating_bar/flutter_rating_bar.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
@@ -16,7 +16,7 @@ class _MapUIStatecustom extends State<MapUIcustom> {
   CustomInfoWindowController _customInfoWindowController =
   CustomInfoWindowController();
 
-  final LatLng _latLng = LatLng(28.7041, 77.1025);
+  final LatLng _latLng = LatLng(39.9333651, 32.859741);
   final double _zoom = 15.0;
   int i_mar = 0;
   bool mapToggle = false;
@@ -35,9 +35,8 @@ class _MapUIStatecustom extends State<MapUIcustom> {
       setState(() {
         currentLocation = currloc;
         mapToggle = true;
-        //populateClients();
+        populateClients();
         i_mar = _markers.length;
-        //populateClients_safeArea();
       });
     });
   }
@@ -48,95 +47,11 @@ class _MapUIStatecustom extends State<MapUIcustom> {
     LatLng(38.4237, 27.1428),
     LatLng(41.0082, 28.9784)
   ];
-  populateClients_safeArea() async {
-    //clients=[];
-    await Firebase.initializeApp();
-    FirebaseFirestore.instance.collection("markers").get().then((docs) {
-      if (docs.docs.isNotEmpty) {
-        for (int i = 0; i < docs.docs.length; ++i) {
-          //clients.add(docs.docs[i].data);
-          loadData_safeArea(docs.docs[i].data, i + i_mar);
-        }
-      }
-    });
-  }
-
-  loadData_safeArea(_latLang, i) {
-    _markers.add(
-      Marker(
-        markerId: MarkerId("$i"),
-        position: LatLng(
-            _latLang()['location'].latitude, _latLang()['location'].longitude),
-        icon: BitmapDescriptor.defaultMarkerWithHue(BitmapDescriptor.hueGreen),
-        onTap: () {
-          _customInfoWindowController.addInfoWindow!(
-            Container(
-              height: 300,
-              width: 200,
-              decoration: BoxDecoration(
-                color: Colors.white,
-                border: Border.all(color: Colors.grey),
-                borderRadius: BorderRadius.circular(10.0),
-              ),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.start,
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Container(
-                    width: 300,
-                    height: 100,
-                    decoration: BoxDecoration(
-                        image: DecorationImage(
-                            image: NetworkImage(_latLang()['image']),
-                            fit: BoxFit.fitWidth,
-                            filterQuality: FilterQuality.high),
-                        borderRadius: const BorderRadius.all(
-                          Radius.circular(10.0),
-                        ),
-                        color: Color.fromARGB(255, 255, 197, 146)),
-                  ),
-                  Padding(
-                    padding:
-                    const EdgeInsets.only(top: 10, left: 10, right: 10),
-                    child: Row(
-                      children: [
-                        SizedBox(
-                          width: 200,
-                          child: Text(
-                            _latLang()['clientName'],
-                            maxLines: 2,
-                            overflow: TextOverflow.fade,
-                            softWrap: false,
-                          ),
-                        ),
-                        const Spacer(),
-                        Text("!!!")
-                      ],
-                    ),
-                  ),
-                  Padding(
-                      padding:
-                      const EdgeInsets.only(top: 10, left: 10, right: 10),
-                      child: Text(
-                        _latLang()['snippet'],
-                        maxLines: 2,
-                      ))
-                ],
-              ),
-            ),
-            LatLng(_latLang()['location'].latitude,
-                _latLang()['location'].longitude),
-          );
-        },
-      ),
-    );
-    setState(() {});
-  }
 
   populateClients() async {
     //clients=[];
     await Firebase.initializeApp();
-    FirebaseFirestore.instance.collection("Status").get().then((docs) {
+    FirebaseFirestore.instance.collection("Doctors").get().then((docs) {
       if (docs.docs.isNotEmpty) {
         for (int i = 0; i < docs.docs.length; ++i) {
           //clients.add(docs.docs[i].data);
@@ -146,12 +61,12 @@ class _MapUIStatecustom extends State<MapUIcustom> {
     });
   }
 
-  loadData(_latLang, i) {
+  loadData(fireBase, i) {
     _markers.add(
       Marker(
         markerId: MarkerId("$i"),
         position: LatLng(
-            _latLang()['location'].latitude, _latLang()['location'].longitude),
+            fireBase()['Location'].latitude, fireBase()['Location'].longitude),
         onTap: () {
           _customInfoWindowController.addInfoWindow!(
             Container(
@@ -167,11 +82,11 @@ class _MapUIStatecustom extends State<MapUIcustom> {
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
                   Container(
-                    width: 300,
+                    width: 350,
                     height: 100,
                     decoration: BoxDecoration(
                         image: DecorationImage(
-                            image: NetworkImage(_latLang()['image']),
+                            image: NetworkImage(fireBase()['Image']),
                             fit: BoxFit.fitWidth,
                             filterQuality: FilterQuality.high),
                         borderRadius: const BorderRadius.all(
@@ -185,16 +100,33 @@ class _MapUIStatecustom extends State<MapUIcustom> {
                     child: Row(
                       children: [
                         SizedBox(
-                          width: 200,
+                          width: 150,
                           child: Text(
-                            _latLang()['address'],
+                            fireBase()['Name'],
                             maxLines: 2,
                             overflow: TextOverflow.fade,
                             softWrap: false,
                           ),
                         ),
                         const Spacer(),
-                        Text("!!!")
+                        //Text(fireBase()['Total_Rating'])
+                        RatingBar.builder( //Rating bar
+                          initialRating: double.parse(fireBase()['Total_Rating']),
+                          itemSize: 20,
+                          minRating: 1,
+                          direction: Axis.horizontal,
+                          wrapAlignment: WrapAlignment.end,
+                          allowHalfRating: true,
+                          itemCount: 5,
+                          itemPadding: EdgeInsets.symmetric(horizontal: 1.0),
+                          itemBuilder: (context, _) => Icon(
+                            Icons.star,
+                            color: Colors.amber,
+                          ),
+                          onRatingUpdate: (rating) {
+                            print(rating);
+                          },
+                        ),
                       ],
                     ),
                   ),
@@ -202,14 +134,37 @@ class _MapUIStatecustom extends State<MapUIcustom> {
                       padding:
                       const EdgeInsets.only(top: 10, left: 10, right: 10),
                       child: Text(
-                        _latLang()['status'],
+                        fireBase()['Description'],
                         maxLines: 2,
-                      ))
+                      )
+                  ),
+                  Padding(
+                      padding:
+                      const EdgeInsets.only(top: 10, left: 10, right: 15,bottom: 10),
+                      child: GestureDetector(
+                        onTap: () {
+                          // Go to doctor page
+                          print('Text Clicked!');
+                        },
+                        child: const Align(
+                          alignment: Alignment.centerRight,
+                          child:Text(
+                            'Explore more!',
+                            style: TextStyle(
+                              fontSize: 15.0,
+                              color: Colors.blue,
+                              decoration: TextDecoration.underline,
+                            ),
+                          ),
+                        )
+                      )
+                  ),
+
                 ],
               ),
             ),
-            LatLng(_latLang()['location'].latitude,
-                _latLang()['location'].longitude),
+            LatLng(fireBase()['Location'].latitude,
+                fireBase()['Location'].longitude),
           );
         },
       ),
@@ -250,13 +205,13 @@ class _MapUIStatecustom extends State<MapUIcustom> {
             },
             markers: _markers,
             initialCameraPosition: CameraPosition(
-              target: LatLng(37.5753, 36.9228),
+              target: LatLng(39.9333651, 32.859741),
               zoom: 5,
             ),
           ),
           CustomInfoWindow(
             controller: _customInfoWindowController,
-            height: 200,
+            height: 225,
             width: 300,
             offset: 35,
           ),
