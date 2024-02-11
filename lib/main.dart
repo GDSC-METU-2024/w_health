f57c5aae802c97e7c9123aa6755b4248e6b75504
mcantimurcan@gmail.com <mcantimurcan@gmail.com>
2024-02-11 19:22:11 +0300

    push notifications configuration 1/2

--- lib/main.dart
+++ lib/main.dart
@@ -1,28 +1,25 @@
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:w_health/screens/login/authPage.dart';
 import 'package:w_health/firebase_options.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+import 'package:w_health/services/push_notifications.dart';
 
 int? initScreen = 0;
-/*
-Future<void> main() async {
-  //WidgetsFlutterBinding.ensureInitialized();
-  //await EasyLocalization.ensureInitialized();
-  //SharedPreferences preferences = await SharedPreferences.getInstance();
-  //initScreen = await preferences.getInt("initScreen");
-  //await preferences.setInt("initScreen", 100);
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
-  runApp(const MyApp());
-}*/
+
+Future<void> handleBackgroundMessaging(RemoteMessage message) async {
+  print(message.data.toString());
+  print(message.notification!.toString());
+}
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
+  LocalNotificationService.initialize();
+  FirebaseMessaging.onBackgroundMessage(handleBackgroundMessaging);
   SharedPreferences preferences = await SharedPreferences.getInstance();
   initScreen = await preferences.getInt("initScreen");
   await preferences.setInt("initScreen", 100);
